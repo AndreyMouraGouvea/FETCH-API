ad3ce22ad36e533749dd7348fb98d50fc4110dec
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-08-04 01:18:48 +0900

    final update

--- www/components/script.js
+++ www/components/script.js
@@ -8,11 +8,15 @@ window.onload = function(){
     mede: 'cors',
     cache: 'default'
   }
-  buscar.addEventListener("click",function(){
+  buscar.addEventListener("click", function(){
    fetch(`http://viacep.com.br/ws/${ cep.value }/json/`, opcoes) //a fetch exige que eu passe uma url, e também que traga parâmetros para esta busca //colocando 'cep.value' para capturar qualquer cep
       .then(response => {response.json() //passando a url, e essas opções... "então" é o que vai acontecer    
       .then(data => {                   //ao enviar os dados, o servidor irá retornar através desta "response"
-        alert("CEP: " +data['cep']+ "\r\nLogradouro: " +data['logradouro']);  //mostrar o cep através do alert
+        // alert("CEP: " +data['cep']+ "\r\nLogradouro: " +data['logradouro']);  //mostrar o cep através do alert
+        document.querySelector("#estado").value = data['uf'];
+        document.querySelector("#cidade").value = data['localidade'];
+        document.querySelector("#bairro").value = data['bairro'];
+        document.querySelector("#rua").value = data['logradouro'];
         })
       })      
   });
