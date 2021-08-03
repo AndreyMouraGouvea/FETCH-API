c72f05c842233035eb3d00b79710742a65ee112a
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-08-04 00:58:15 +0900

    update

--- www/components/script.js
+++ www/components/script.js
@@ -1,13 +1,15 @@
 window.onload = function(){
   const buscar = document.querySelector("#buscar");
 
+  const cep = document.querySelector("#cep");
+
   const opcoes = {
     method: 'GET',
     mede: 'cors',
     cache: 'default'
   }
   buscar.addEventListener("click",function(){
-   fetch('http://viacep.com.br/ws/11750000/json/', opcoes) //a fetch exige que eu passe uma url, e também que traga parâmetros para esta busca
+   fetch(`http://viacep.com.br/ws/${ cep.value }/json/`, opcoes) //a fetch exige que eu passe uma url, e também que traga parâmetros para esta busca //colocando 'cep.value' para capturar qualquer cep
       .then(response => {response.json() //passando a url, e essas opções... "então" é o que vai acontecer    
       .then(data => {                   //ao enviar os dados, o servidor irá retornar através desta "response"
         alert("CEP: " +data['cep']+ "\r\nLogradouro: " +data['logradouro']);  //mostrar o cep através do alert
